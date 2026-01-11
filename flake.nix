e93d8fb8300eb26b67d4c7f4619b740ae682e696
installer <installer@gmail.com>
2026-01-11 17:41:14 +0100

    nixified repo

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Haskell Monorepo";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          haskellPackages.ghc
+          haskellPackages.cabal-install
+          haskellPackages.haskell-language-server
+        ];
+        shellHook = "echo '🛡️ 🔥  🥵 Haskell flake active! \n fun learning!'";
+      };
+    };
+}
